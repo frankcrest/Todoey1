8c8335edc71b0b1da9eccaa868ace06fd523990a
frankcrest <frankcrestview@hotmail.com>
2019-01-15 22:32:59 -0500

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Frank Chen on 2019-01-15.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+}
